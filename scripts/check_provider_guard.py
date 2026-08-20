a57e2830daa5b0e9da32cab39f22c548bb1bff86
bzimm <bzimm@users.noreply.github.com>
2026-08-19 21:48:26 -0400

    chore: add provider-guard runtime diagnostics script

---
+++ scripts/check_provider_guard.py
@@ -0,0 +1,22 @@
+﻿import os
+import time
+from joi_companion.core.provider_guard import enforce_free_first_provider, has_paid_authorization
+
+provider = os.getenv("AURION_LLM_PROVIDER", "")
+chosen = enforce_free_first_provider(provider)
+
+raw_exp = os.getenv("AURION_PAID_AUTH_EXPIRES_UNIX", "")
+now = int(time.time())
+exp = None
+try:
+    exp = int(raw_exp) if raw_exp else None
+except Exception:
+    exp = None
+
+print("provider_env=", provider or "<empty>", sep="")
+print("provider_chosen=", chosen, sep="")
+print("paid_token_set=", "yes" if bool(os.getenv("AURION_PAID_AUTH_TOKEN")) else "no", sep="")
+print("paid_auth_valid=", "yes" if has_paid_authorization() else "no", sep="")
+print("now_unix=", now, sep="")
+print("paid_exp_unix=", exp if exp is not None else "<none>", sep="")
+print("seconds_remaining=", (exp - now) if isinstance(exp, int) else "<none>", sep="")
